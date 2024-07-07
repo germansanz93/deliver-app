3654c6872aba169cb10adbfa45671c88abb78080
German Sanz <germansanz93@gmail.com>
2024-07-06 23:10:44 -0300

    added dynamic navbar title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -16,6 +18,7 @@ import NextLink from "next/link";
 import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
+import { customerConfig, CustomerConfig } from "@/config/customers";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
   TwitterIcon,
@@ -25,8 +28,11 @@ import {
   SearchIcon,
   Logo,
 } from "@/components/icons";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   const searchInput = (
     <Input
       aria-label="Search"
@@ -34,13 +40,13 @@ export const Navbar = () => {
         inputWrapper: "bg-default-100",
         input: "text-sm",
       }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
+      // endContent={
+      //   <Kbd className="hidden lg:inline-block" keys={["command"]}>
+      //     K
+      //   </Kbd>
+      // }
       labelPlacement="outside"
-      placeholder="Search..."
+      placeholder="Buscar producto"
       startContent={
         <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
       }
@@ -54,7 +60,7 @@ export const Navbar = () => {
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">{customerConfig.customers.find(c => c.href == pathname)?.name || "11b"}</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -80,19 +86,19 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
+          {/* <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
             <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
+          </Link> */}
+          {/* <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
             <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+          </Link> */}
+          {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
-          </Link>
+          </Link> */}
           <ThemeSwitch />
         </NavbarItem>
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
+        {/* <NavbarItem className="hidden md:flex">
           <Button
             isExternal
             as={Link}
@@ -103,13 +109,13 @@ export const Navbar = () => {
           >
             Sponsor
           </Button>
-        </NavbarItem>
+        </NavbarItem> */}
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+        {/* <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
-        </Link>
+        </Link> */}
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
---
+++ config/customers.ts
@@ -0,0 +1,22 @@
+export type CustomerConfig = typeof customerConfig;
+
+export const customerConfig = {
+  customers: [
+    {
+      name: "Lo de Sanz",
+      description: "Lo de Sanz is a restaurant in Buenos Aires, Argentina.",
+      href: "/lo-de-sanz",
+    },
+    {
+      name: "Pizzeria Guerrin",
+      description: "Pizzeria Guerrin is a pizzeria in Buenos Aires, Argentina.",
+      href: "/pizzeria-guerrin",
+    },
+    {
+      name: "El Preferido de Palermo",
+      description:
+        "El Preferido de Palermo is a restaurant in Buenos Aires, Argentina.",
+      href: "/el-preferido-de-palermo",
+    },
+  ],
+};
